06f7b21e8b33e9261d11079416dcfcc40ff0ad28
hizkiarafa <169180191+hizkiarafa@users.noreply.github.com>
2024-05-14 14:14:30 +0800

    Add files via upload

---
+++ web-kia/script.js
@@ -0,0 +1,18 @@
+// toggle class active
+const navbarnav = document.querySelector
+('.navbar-nav');
+//ketika humberger menu di klik
+document.querySelector('#humberger-menu').
+onclick = () =>{
+    navbarnav.classList.toggle('active');
+};
+
+// klik di luar untuk menghilangkan nav
+const humberger = document.querySelector
+('#humberger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!humberger.contains(e.target) && !navbarnav.contains(e.target)){
+        navbarnav.classList.remove('active');
+    }
+});
